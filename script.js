64434b7704cc0a8fe9cbeec64dceaad6ccc54a63
hugoresende27 <Hugoresende27@gmail.com>
2021-11-10 18:48:50 +0000

    a

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-// const tagsEl = document.getElementById('tags')
-// const texto = document.getElementById('txtArea')
+const tagsEl = document.getElementById('tags')
+const texto = document.getElementById('txtArea')
 
-// texto.focus()/* QUANDO CARREGA A PÁGINA O CURSOR FICA A PISCAR NA TEXTBOX, FOCUS */
+texto.focus()/* QUANDO CARREGA A PÁGINA O CURSOR FICA A PISCAR NA TEXTBOX, FOCUS */
 
-// texto.addEventListener('keyup', (e) => {/*FUNÇÃO QUANDO SE SOLTA A TECLA */
-//     criarTags(e.target.value)/*FUNÇÃO criarTags CONSOLE.LOG DA TECLA DE 'keyup' */
-// })
+texto.addEventListener('keyup', (e) => {/*FUNÇÃO QUANDO SE SOLTA A TECLA */
+    criarTags(e.target.value)/*FUNÇÃO criarTags CONSOLE.LOG DA TECLA DE 'keyup' */
+
+    if(e.key === 'Enter') {
+        setTimeout( () => {
+            e.target.value = ''
+        }, 10 )/*ACONTECE A CADA 10ms */
+
+        escolhaRandom()
+    }
+})
 /*
 ## The split() method splits a string into an array of substrings, and returns the array.
 If (" ") is used as separator, the string is split between words.
@@ -14,89 +22,56 @@ If (" ") is used as separator, the string is split between words.
 ## The trim() method removes whitespace from both ends of a string. Whitespace in this context is all the whitespace characters (space, tab, no-break space, etc.) and all the line terminator characters (LF, CR, etc.).
 */
 
-// function criarTags ( input ) {
-//     console.log(input)
-//     const tags = input.split(',')/*VAI CRIAR UM ARRAY CADA VIRGULA 1 INDEX */
-//     .filter(tag => tag.trim() !== '')
-//     .map(tag=>tag.trim()) /*TRIM NOS ESPAÇOS EM BRANCO E NO CASO DE , ,  */
-//     console.log(tags)
+function criarTags ( input ) {
+    console.log(input)
+    const tags = input.split(',')/*VAI CRIAR UM ARRAY CADA VIRGULA 1 INDEX */
+    .filter(tag => tag.trim() !== '')
+    .map(tag=>tag.trim()) /*TRIM NOS ESPAÇOS EM BRANCO E NO CASO DE , ,  */
+    console.log(tags)
 
-//     texto.innerHTML='' /*LIMPAR AS TAGS */
+    tagsEl.innerHTML='' /*LIMPAR AS TAGS */
     
-//     tags.forEach(tag => {
-//         const tagEl = document.createElement('span')
-//         tagEl.classList.add('tag')
-//         tagEl.innerText = tag
-//         tagsEl.appendChild(tagEl)
-//     })
-
-// }
-const tagsEl = document.getElementById('tags')
-const textarea = document.getElementById('txtArea')
-
-textarea.focus()
-
-textarea.addEventListener('keyup', (e) => {
-    createTags(e.target.value)
-
-    if(e.key === 'Enter') {
-        setTimeout(() => {
-            e.target.value = ''
-        }, 10)
-
-        randomSelect()
-    }
-})
-
-function createTags(input) {
-    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-    
-    tagsEl.innerText = ''
-
     tags.forEach(tag => {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
+
 }
 
-function randomSelect() {
-    const times = 30
+function escolhaRandom () {
+    //console.log(123)
+    const vezes = 30
 
-    const interval = setInterval(() => {
+    const intervalo = setInterval ( () => {
         const randomTag = pickRandomTag()
-	
-	if (randomTag !== undefined) {
-        highlightTag(randomTag)
+    
+        highlighTag(randomTag)
+        setTimeout( () => {
+            unHighlighTag(randomTag)
+        },100)
+    
+    }, 100)/*ACONTECE A CADA 100ms */
 
-        setTimeout(() => {
-            unHighlightTag(randomTag)
-        }, 100)
-	}
-    }, 100);
+    setTimeout( () => {
+        clearInterval(intervalo)
 
-    setTimeout(() => {
-        clearInterval(interval)
-
-        setTimeout(() => {
+        setTimeout( ()=> {
             const randomTag = pickRandomTag()
-
-            highlightTag(randomTag)
-        }, 100)
-
-    }, times * 100)
+            highlighTag(randomTag)
+        },100)
+    },vezes * 100)
 }
 
-function pickRandomTag() {
+function pickRandomTag(){
     const tags = document.querySelectorAll('.tag')
-    return tags[Math.floor(Math.random() * tags.length)]
+    return tags [Math.floor(Math.random()*tags.length)]
 }
 
-function highlightTag(tag) {
-    tag.classList.add('highlight')
+function highlighTag(tag){
+    tag.classList.add('realce')
 }
-
-function unHighlightTag(tag) {
-    tag.classList.remove('highlight')
+function unHighlighTag(tag){
+    tag.classList.remove('realce')
 }
